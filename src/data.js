cca4cc45beb697e60615e79c4b7ff280d872debe
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-08-10 00:52:48 -0700

    start

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const month = [
+    {
+        price:'0',
+        text:'month'
+    },
+    {
+        price:'23',
+        text:'month'
+    },
+    {
+        price:'89',
+        text:'month'
+    },
+]
+
+export const year = [
+    {
+        price :'0',
+        text:'year'
+    },
+    {
+        price :'199',
+        text:'year'
+    },
+    {
+        price :'799',
+        text:'year'
+    },
+]
